bfcf897ceb7e5f792b752c02c2e7baf92ce94add
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-02-03 01:24:55 +0800

    finalcodes4

--- src/dropdownList.js
+++ src/dropdownList.js
@@ -2,7 +2,7 @@ const dropdownList = [
   { name: "1ST MINI ALBUM 'FEARLESS'" },
   { name: "2ND MINI ALBUM 'ANTIFRAGILE'" },
   { name: "1ST STUDIO ALBUM 'UNFORGIVEN'" },
-  { name: "PERFECT NIGHT' SINGLE" },
+  { name: "'PERFECT NIGHT' SINGLE" },
   { name: "3RD MINI ALBUM 'EASY'" },
 ];
 
